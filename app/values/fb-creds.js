bbff70a6623c57aeb2c96fe3634c559e3764ed2e
Dylan Smith <jeffdylansmith@gmail.com>
2017-08-24 21:31:54 -0500

    adding fb creds

---
+++ app/values/fb-creds.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ app.constant('FBcreds', {
+    apiKey: "AIzaSyCCMDq477S2rO8E1nxzIj63b88ILnleEEs",
+    authDomain: "fecapstone-8eafd.firebaseapp.com",
+    databaseURL: "https://fecapstone-8eafd.firebaseio.com"
+ });
